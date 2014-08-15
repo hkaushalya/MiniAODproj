e3099f344b3399ba0d3108bac089ec55d8745545
samantha <samantha@cern.ch>
2014-08-14 22:16:42 -0500

    added vector with pair

--- src/myLinkDef.h
+++ src/myLinkDef.h
@@ -1,7 +1,9 @@
 #include <vector>
 #include <string>
+#include <utility>
 #ifdef __MAKECINT__
 #include <TLorentzVector.h>
+#pragma link C++ class std::vector< std::pair<unsigned, unsigned> >+;
 #pragma link C++ class std::vector<TLorentzVector>+;
 #pragma link C++ class std::vector<int>+;
 #pragma link C++ class std::vector<double>+;
